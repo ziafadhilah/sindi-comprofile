58a7090f169208fb4d11f50e5f7d074100d275e3
ziafadhilah <ziafadhilah98@gmail.com>
2024-07-18 14:38:18 +0700

    home page done

--- js/index.js
+++ js/index.js
@@ -73,6 +73,36 @@ fetch("pages/how-to.html")
     console.error("There was a problem with the fetch operation:", error);
   });
 
+//get join.html
+fetch("pages/join.html")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.text();
+  })
+  .then((data) => {
+    document.getElementById("joinContent").innerHTML = data;
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
+
+//get footer.html
+fetch("layouts/footer.html")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.text();
+  })
+  .then((data) => {
+    document.getElementById("footerContent").innerHTML = data;
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
+
 // Event listener untuk gambar yg mengikuti cursor
 document.addEventListener("DOMContentLoaded", function () {
   document.addEventListener("mousemove", function (e) {
